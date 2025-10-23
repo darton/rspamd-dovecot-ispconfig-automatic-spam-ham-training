782db4be3cd4d290d2331057dba15048b5f41f34
Dariusz Kowalczyk <darton@users.noreply.github.com>
2025-10-23 18:33:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
 
-cp dovecot_custom.conf.master  /usr/local/ispconfig/server/conf-custom/install/
+set -e  # zakończ przy pierwszym błędzie
+
+echo "🔧 Instalacja expect..."
+sudo apt update
+sudo apt install -y expect
+
+echo "📁 Kopiowanie pliku konfiguracyjnego dovecot..."
+sudo cp dovecot_custom.conf.master /usr/local/ispconfig/server/conf-custom/install/
+
+echo "🔐 Nadanie uprawnień do skryptu expect..."
+sudo chmod +x ispconfig_auto_update.exp
+
+echo "🚀 Uruchamianie automatycznej aktualizacji ISPConfig..."
+sudo ./ispconfig_auto_update.exp
